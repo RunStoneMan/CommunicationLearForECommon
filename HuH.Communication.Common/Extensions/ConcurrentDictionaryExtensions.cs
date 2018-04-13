feca0aac0a72e587dd444934cb7c5fd1cb276be3
lockness <hu_han_2007@hotmail.com>
2018-04-13 11:23:48 +0800

    common

---
+++ HuH.Communication.Common/Extensions/ConcurrentDictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuH.Communication.Common.Extensions
+{
+    public static class ConcurrentDictionaryExtensions
+    {
+        public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
+        {
+            TValue value;
+            return dict.TryRemove(key, out value);
+        }
+    }
+}
