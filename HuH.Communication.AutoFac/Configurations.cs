546a610eec64a77d43a43460221f50e7c36396f7
lockness <hu_han_2007@hotmail.com>
2018-04-19 09:28:45 +0800

    c

---
+++ HuH.Communication.AutoFac/Configurations.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using HuH.Communication.Common;
+using HuH.Communication.Common.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuH.Communication.AutoFac
+{
+   public static class Configurations
+    {
+        public static Workbench UseAutofac(this Workbench configuration)
+        {
+            return UseAutofac(configuration, new ContainerBuilder());
+        }
+        /// <summary>Use Autofac as the object container.
+        /// </summary>
+        /// <returns></returns>
+        public static Workbench UseAutofac(this Workbench configuration, ContainerBuilder containerBuilder)
+        {
+            ObjectContainer.SetContainer(new AutofacObjectContainer(containerBuilder));
+            return configuration;
+        }
+    }
+}
