9004dd1b4e946b4a57d92d9fddf200d51702135e
lockness <hu_han_2007@hotmail.com>
2018-04-19 09:35:37 +0800

    c

---
+++ HuH.Communication.Common/Exceptions/ApplicationInitializationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuH.Communication.Common.Exceptions
+{
+    public class ApplicationInitializationException : Exception
+    {
+        public ApplicationInitializationException(string message) : base(message) { }
+        public ApplicationInitializationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ HuH.Communication.Common/Utils/ByteOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuH.Communication.Common.Utils
+{
+    public enum ByteOrder
+    {
+        /// <summary>
+        ///     Default on most Windows systems
+        /// </summary>
+        LittleEndian = 0,
+        BigEndian = 1
+    }
+}
